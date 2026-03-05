e8d83f4cc0b53a64beb403ed96252dc5e14e43c7
Lohi2005 <lohitha252005@gmail.com>
2026-03-05 19:27:26 +0530

    app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, request, jsonify
-from chatbot import get_response
+from flask import Flask, render_template, request, jsonify
+from chatbot import get_chatbot_response
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AI Chatbot is Running!"
+    return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    user_message = request.json.get("message")
-    response = get_response(user_message)
-    return jsonify({"response": response})
+    user_input = request.json.get("message")
+    response = get_chatbot_response(user_input)
+    return jsonify({"reply": response})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
